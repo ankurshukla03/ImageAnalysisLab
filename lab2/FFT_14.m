813b1850c5a439cc7262c7fb21d89dff539650a4
Ankur Shukla <ankur.shukla.7818@student.uu.se>
2017-11-27 14:21:38 +0100

    merge in lab 2

---
+++ lab2/FFT_14.m
@@ -0,0 +1,7 @@
+f = imread('freqdist.png');
+
+
+%F1 = fft2(F);
+%F2 = fftshift(F1);
+%imshow(F);
+%subplot(2,2,2),imshow(F2);
--- lab2/FFT_cameraman.m
+++ lab2/FFT_cameraman.m
@@ -2,19 +2,48 @@
 Cman = imread('images/cameraman.png');
 Cman = double(Cman);
 
-I2 = fft2(Cman);
-% I3 = fftshift(I2);
-% I3 = abs(I3);
-% I3 = log(I3);
-% 
-[rows, cols] = size(I3);
-result = uint8(zeros(rows, cols));
-for r = 1:rows
-    for c = 1:cols
-        result(r, c) = uint8(I3(r,c) < 0);
-    end
-end
 
-imagesc(result);
-pause;
-imagesc(ifft2(result));
+% 
+%[rows, cols] = size(I3);
+%result = uint8(zeros(rows, cols));
+%for r = 1:rows
+%    for c = 1:cols
+%        result(r, c) = uint8(I3(r,c) < 0);
+%    end
+%end
+
+
+I2 = fft2(Cman);
+Img = fftshift(I2);
+% 
+% 
+% for filters={'gaussian'}
+%    idx = 1;
+%    fname = filters{1};
+%    for fsize=[3,7,31]
+%       if (strcmp(fname,'gaussian'))
+%           mFilter = fspecial(fname, fsize, fsize/2);
+%       else
+%           mFilter = fspecial(fname, fsize);
+%       end
+% 
+%       Smooth_Img = imfilter(Img, mFilter, 'same');
+%       Sharp_Img = Img + 0.5 * (Img - Smooth_Img);
+% 
+%       subplot(3,3,idx), imshow(Img);
+%       subplot(3,3,idx + 1), imshow(Smooth_Img);
+%       subplot(3,3,idx + 2), imshow(Sharp_Img);
+%       title(fname);
+%       
+%       idx = idx + 3;
+%    end
+%    end
+
+% 
+% Img1 = ifft2(Img);
+% imagesc(Img1);
+ 
+%imagesc(I3);
+%pause;
+%imagesc(log(ifftshift(ifft2(result))));
+
