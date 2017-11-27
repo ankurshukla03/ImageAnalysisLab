6638ffc5f3cd114c0bd08b0c44b17d14b8211a17
Tristan <thrawst88@gmail.com>
2017-11-27 13:51:12 +0100

    added morph element to lab3

--- lab3/segmentation.m
+++ lab3/segmentation.m
@@ -2,14 +2,17 @@
 
 % read image
 Img = imread('coins.tif');
-Img = Img(1:200,101:300); %smaller image, 14 coins
+% Img = Img(1:200,101:300); %smaller image, 14 coins
 
-% mean filter before more processing
+% mean filter, clean noise
 Img = imfilter(Img,[1 1 1; 1 1 1; 1 1 1]./9);
 
 % get threshold for grayscale
 thresh = graythresh(Img);
 Bw_Img = ~im2bw(Img, thresh);
+
+% close holes 
+Bw_Img = bwmorph(Bw_Img, 'close');
 
 % distance
 Dist_Img = bwdist(~Bw_Img);
@@ -36,7 +39,9 @@ subplot(rows,cols,2), imshow(Dist_Img);
 subplot(rows,cols,3), imshow(label2rgb(Ws_Img, 'spring'));
 
 % _____________ row 2
+
 % label regions
+% for each centroid, put a label there.
 subplot(rows,cols,4), imshow(Img);
 hold on
 for k = 1:numel(F)
@@ -45,6 +50,7 @@ for k = 1:numel(F)
 end
 hold off
 
+% histogram
 fname = fieldnames(F); % keeping the property name abstract
 regions = [ F.(fname{1}) ];
 subplot(rows,cols,5), hist(regions);
