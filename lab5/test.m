a1cdad0594cdf615506c7b5734610519ef627e77
Tristan <thrawst88@gmail.com>
2017-12-31 15:13:17 +0100

    added test.m

---
+++ lab5/test.m
@@ -0,0 +1,26 @@
+Img = imread('imagedata/train_0582.png');
+
+thresh = graythresh(Img);
+Img = im2bw(Img, thresh);
+
+% mean filter
+Img = imfilter(Img, ones(5,5)./25);
+
+% close elements
+Img = bwmorph(Img, 'close');
+
+Dist_Img = bwdist(~Img);
+Dist_Img = -Dist_Img;
+
+Ws_Img = watershed(Dist_Img);
+Ws_Img(~Img) = 0;
+
+Lb_Img = bwlabel(Ws_Img,8);
+
+rows = 1;
+cols = 4;
+subplot(rows,cols,1), imshow(Img);
+subplot(rows,cols,2), imagesc(Dist_Img);
+subplot(rows,cols,3), imshow(label2rgb(Ws_Img, 'spring'));
+subplot(rows,cols,4), imshow(label2rgb(Lb_Img, 'spring'));
+
